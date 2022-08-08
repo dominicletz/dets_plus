ba6ab306827cf1f320577de9806bafd85d7e86ad
Dominic Letz <dominic@diode.io>
2022-08-08 13:49:36 +0200

    Updated paged_file

--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule DetsPlus.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :crypto]
     ]
   end
 
