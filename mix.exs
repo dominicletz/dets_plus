9b3516bdb589a1d3ce727fc5338c80b29fc19801
Dominic Letz <dominic.letz@berlin.de>
2024-06-24 11:16:13 +0200

    v2.3.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule DetsPlus.MixProject do
   use Mix.Project
 
-  @version "2.2.0"
+  @version "2.3.0"
   @name "DetsPlus"
   @url "https://github.com/dominicletz/dets_plus"
   @maintainers ["Dominic Letz"]
